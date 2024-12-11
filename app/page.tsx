9182b1b9c6efaa444ab36938553c65d6638b470d
Bernardo Ferreira <122024961+marujoo@users.noreply.github.com>
2024-12-11 04:45:49 +0000

    feito

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function page() {
   return <section className='home'>
-  <p>Estou muito entusiasmado por dar vida a este projeto usando tecnologias tão incríveis como React e Next.js. Esta jornada tem sido uma mistura de aprendizado, desafios e muita evolução.
+  <p>Estou muito entusiasmado por dar vida a este projeto usando tecnologias tão incríveis como React e Next.js. Esta jornada tem sido uma mistura de aprendizagem, desafios e muita evolução.
     Cada funcionalidade criada é um passo a mais na direção dos meus objetivos, e estou animado para descobrir tudo o que o futuro reserva para este projeto.
-    Obrigado por estar aqui e fazer parte desta aventura. Vamos juntos construir algo extraordinário! 🚀💻✨
+    Obrigado por estares aqui e fazer parte desta aventura. Vamos juntos construir algo extraordinário! 🚀💻✨
     </p>
   </section>
 }
